89bd76024802ffaa7614330fc6fecb850e1a4216
Naveen3873 <naveenbe3873@gmail.com>
2021-09-30 18:57:39 +0530

    form input data stored in DB adn retrive from table

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,32 +21,20 @@ public class StudentController {
 	@Autowired
 	public StudentRepository StudentRepo;
 	
-	@GetMapping(value = "/insert")
-	public ResponseEntity<?> getStudent(){
-		Student n1 = new Student();
-		n1.setName("Naveen");
-		n1.setAge(23);
-		n1.setGender('M');
-		n1.setEmail("abc12@gmail.com");
+	@PostMapping(value = "/insert")
+	public ResponseEntity<?> getStudent(@RequestBody final Student n1){
 		StudentRepo.save(n1);
-		/*
-		Student n2 = new Student();
-		n2.setName("Jana");
-		n2.setAge(22);
-		n2.setGender('M');
-		n2.setEmail("ahd67@gmail.com");
-		
-		Student n3 = new Student();
-		n3.setName("raj");
-		n3.setAge(23);
-		n3.setGender('M');
-		n3.setEmail("andh21@gmail.com");
-		
-		Object[] obj = { n1,n2,n3};
-		*/
 		return ResponseEntity
 				.status(HttpStatus.OK)
-				.body("Your value is stored");
+				.body("insert successfully");
 		
+	}
+	
+	@GetMapping(value = "/getAll")
+	public ResponseEntity<?> getAllStudents(){
+		ArrayList<Student> students = (ArrayList<Student>) StudentRepo.findAll();
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(students);
 	}
 }
