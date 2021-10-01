fff01854df7b0b5d9c616c09d5f91b5cb041e9e4
m-meena <meena58apt@gmail.com>
2021-10-01 17:59:30 +0530

    delete method and update method created

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +37,31 @@ public class StudentController {
 		return ResponseEntity
 				.status(HttpStatus.OK)
 				.body(students);
+	}
+	
+	@GetMapping(value = "/delete/{id}")
+	public ResponseEntity<?> deleteStudent(@PathVariable final int id){
+		StudentRepo.deleteById(id);
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body("Student deleted Successfully!");
+		
+	}
+	
+	@GetMapping(value = "/get/{id}")
+	public ResponseEntity<?> getStudent(@PathVariable final int id){
+		Student student = StudentRepo.findById(id).get();
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(student);
+	}	
+	
+	@PostMapping(value="/update")
+	public ResponseEntity<?> updateStudent(@RequestBody final Student s){
+		StudentRepo.save(s);
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body("Student saved successfully!");
+		
 	}
 }
